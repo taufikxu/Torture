b3e44d054a59ade4b01d28b312acda2ccee5fdea
taufikxu <kunxu.thu@gmail.com>
2020-07-30 11:26:12 +0800

    update

--- utils/checkpoint.py
+++ utils/checkpoint.py
@@ -4,30 +4,31 @@ from torch.utils import model_zoo
 
 
 class CheckpointIO(object):
-    ''' CheckpointIO class.
+    """ CheckpointIO class.
 
     It handles saving and loading checkpoints.
 
     Args:
         checkpoint_dir (str): path where checkpoints are saved
-    '''
-    def __init__(self, checkpoint_dir='./chkpts', **kwargs):
+    """
+
+    def __init__(self, checkpoint_dir="./chkpts", **kwargs):
         self.module_dict = kwargs
         self.checkpoint_dir = checkpoint_dir
         if not os.path.exists(checkpoint_dir):
             os.makedirs(checkpoint_dir)
 
     def register_modules(self, **kwargs):
-        ''' Registers modules in current module dictionary.
-        '''
+        """ Registers modules in current module dictionary.
+        """
         self.module_dict.update(kwargs)
 
     def save(self, filename, **kwargs):
-        ''' Saves the current module dictionary.
+        """ Saves the current module dictionary.
 
         Args:
             filename (str): name of output file
-        '''
+        """
         if not os.path.isabs(filename):
             filename = os.path.join(self.checkpoint_dir, filename)
 
@@ -37,10 +38,10 @@ class CheckpointIO(object):
         torch.save(outdict, filename)
 
     def load_file(self, filename, base_name=None):
-        '''Loads a module dictionary from file.
+        """Loads a module dictionary from file.
         Args:
             filename (str): name of saved module dictionary
-        '''
+        """
         base_name = base_name if base_name is not None else self.checkpoint_dir
         if not os.path.isabs(filename):
             filename = os.path.join(base_name, filename)
@@ -50,32 +51,30 @@ class CheckpointIO(object):
             scalars = self.parse_state_dict(state_dict)
             return scalars
         else:
+            print(filename)
             raise FileNotFoundError
 
     def load_url(self, url):
-        '''Load a module dictionary from url.
+        """Load a module dictionary from url.
         Args:
             url (str): url to saved model
-        '''
+        """
         print(url)
-        print('=> Loading checkpoint from url...')
+        print("=> Loading checkpoint from url...")
         state_dict = model_zoo.load_url(url, progress=True)
         scalars = self.parse_state_dict(state_dict)
         return scalars
 
     def parse_state_dict(self, state_dict):
-        '''Parse state_dict of model and return scalars.
+        """Parse state_dict of model and return scalars.
         Args:
             state_dict (dict): State dict of model
-        '''
+        """
 
         for k, v in self.module_dict.items():
             if k in state_dict:
                 v.load_state_dict(state_dict[k])
             else:
-                print('Warning: Could not find %s in checkpoint!' % k)
-        scalars = {
-            k: v
-            for k, v in state_dict.items() if k not in self.module_dict
-        }
+                print("Warning: Could not find %s in checkpoint!" % k)
+        scalars = {k: v for k, v in state_dict.items() if k not in self.module_dict}
         return scalars
